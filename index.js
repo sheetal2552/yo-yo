73dcac97c2dc96feb51b9d4e375c965e61e7f143
sheetal2552 <sheetalupwork2552@gmail.com>
2016-03-11 15:02:07 -0800

    bel@4.0.0 includes hyperx

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-var hyperx = require('hyperx') // turns template tag output into the DOM builder API
-var bel = require('bel') // turns DOM builder arguments into DOM elements
-module.exports = hyperx(bel)
+module.exports = require('bel') // turns template tag into DOM elements
 module.exports.update = require('morphdom') // efficiently diffs + morphs two DOM elements
