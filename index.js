27b1dd1e34ea8faaeddeff1b47f2ef8ed6494736
sheetal2552 <sheetalupwork2552@gmail.com>
2016-03-14 21:19:19 -0700

    style

--- index.js
+++ index.js
@@ -7,50 +7,50 @@ module.exports.update = function (fromNode, toNode, opts) {
   if (!opts) opts = {}
   if (!opts.onBeforeMorphEl) opts.onBeforeMorphEl = copyEvents
   if (!opts.onBeforeMorphElChildren) opts.onBeforeMorphElChildren = copyEvents
+
   morphdom(fromNode, toNode, opts)
+
   function copyEvents (f, t) {
-    var events = opts.event || defaultEvents
+    var events = opts.events || defaultEvents
     for (var i = 0; i < events.length; i++) {
       var ev = events[i]
-      if (t[ev]) {
-        f[ev] = t[ev]
-      }
+      if (t[ev]) f[ev] = t[ev]
     }
   }
 }
 
 var defaultEvents = [
   // attribute events (can be set with attributes)
-  "onclick",
-  "ondblclick",
-  "onmousedown",
-  "onmouseup",
-  "onmouseover",
-  "onmousemove",
-  "onmouseout",
-  "ondragstart",
-  "ondrag",
-  "ondragenter",
-  "ondragleave",
-  "ondragover",
-  "ondrop",
-  "ondragend",
-  "onkeydown",
-  "onkeypress",
-  "onkeyup",
-  "onunload",
-  "onabort",
-  "onerror",
-  "onresize",
-  "onscroll",
-  "onselect",
-  "onchange",
-  "onsubmit",
-  "onreset",
-  "onfocus",
-  "onblur",
+  'onclick',
+  'ondblclick',
+  'onmousedown',
+  'onmouseup',
+  'onmouseover',
+  'onmousemove',
+  'onmouseout',
+  'ondragstart',
+  'ondrag',
+  'ondragenter',
+  'ondragleave',
+  'ondragover',
+  'ondrop',
+  'ondragend',
+  'onkeydown',
+  'onkeypress',
+  'onkeyup',
+  'onunload',
+  'onabort',
+  'onerror',
+  'onresize',
+  'onscroll',
+  'onselect',
+  'onchange',
+  'onsubmit',
+  'onreset',
+  'onfocus',
+  'onblur',
   // other common events
-  "oncontextmenu",
-  "onfocusin",
-  "onfocusout"
+  'oncontextmenu',
+  'onfocusin',
+  'onfocusout'
 ]
