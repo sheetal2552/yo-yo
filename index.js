31d5881e0856ae8a7518d74992e3ef1e61af2a9b
sheetal2552 <sheetalupwork2552@gmail.com>
2016-03-14 21:15:53 -0700

    add default list of events to copy

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
-module.exports = require('bel') // turns template tag into DOM elements
-module.exports.update = require('morphdom') // efficiently diffs + morphs two DOM elements
+var bel = require('bel') // turns template tag into DOM elements
+var morphdom = require('morphdom') // efficiently diffs + morphs two DOM elements
+
+module.exports = bel
+
+module.exports.update = function (fromNode, toNode, opts) {
+  if (!opts) opts = {}
+  if (!opts.onBeforeMorphEl) opts.onBeforeMorphEl = copyEvents
+  if (!opts.onBeforeMorphElChildren) opts.onBeforeMorphElChildren = copyEvents
+  morphdom(fromNode, toNode, opts)
+  function copyEvents (f, t) {
+    var events = opts.event || defaultEvents
+    for (var i = 0; i < events.length; i++) {
+      var ev = events[i]
+      if (t[ev]) {
+        f[ev] = t[ev]
+      }
+    }
+  }
+}
+
+var defaultEvents = [
+  // attribute events (can be set with attributes)
+  "onclick",
+  "ondblclick",
+  "onmousedown",
+  "onmouseup",
+  "onmouseover",
+  "onmousemove",
+  "onmouseout",
+  "ondragstart",
+  "ondrag",
+  "ondragenter",
+  "ondragleave",
+  "ondragover",
+  "ondrop",
+  "ondragend",
+  "onkeydown",
+  "onkeypress",
+  "onkeyup",
+  "onunload",
+  "onabort",
+  "onerror",
+  "onresize",
+  "onscroll",
+  "onselect",
+  "onchange",
+  "onsubmit",
+  "onreset",
+  "onfocus",
+  "onblur",
+  // other common events
+  "oncontextmenu",
+  "onfocusin",
+  "onfocusout"
+]
